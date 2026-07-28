929c2ab722461f673ecdf39ab5d738d42e298f7d
Bruno Gerotto <bedgerotto@gmail.com>
2026-07-28 21:27:01 +0100

    fix: initialize Solid databases in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
   config.cache_store = :solid_cache_store
   config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
   config.action_cable.mount_path = "/cable"
   config.active_storage.service = :local
 end
